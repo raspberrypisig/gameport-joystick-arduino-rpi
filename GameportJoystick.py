6ff1e07cb02c9e2ef77118e70c858a4385bee43f
raspberrypisig <raspberrypisig@gmail.com>
2018-09-23 18:29:13 +1000

    Update GameportJoystick.py

--- GameportJoystick.py
+++ GameportJoystick.py
@@ -1,9 +1,10 @@
-from evdev import UInput, ecodes as e
+from evdev import UInput, AbsInfo, ecodes as e
 from time import sleep
 
 # https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
 
 # This script MUST be run as root
+
 
 class GameportJoystick:
   caps = {
@@ -11,7 +12,8 @@ class GameportJoystick:
       e.BTN_TRIGGER, e.BTN_THUMB
   ],
     e.EV_ABS: [
-      e.ABS_X, e.ABS_Y
+      (e.ABS_X, AbsInfo(value=0, min=0, max=1024, fuzz=0, flat=0, resolution=0)),
+      (e.ABS_Y, AbsInfo(value=0, min=0, max=1024, fuzz=0, flat=0, resolution=0))
   ],
   }
 
@@ -23,6 +25,13 @@ class GameportJoystick:
     self.ui.write(type, key, value)
     self.ui.syn()
 
+  def pressAndHoldTriggerButton(self):
+    toggle=0
+    for i in range(1000):
+      self.ui.write(e.EV_KEY, e.BTN_TRIGGER, toggle)
+      self.ui.syn()
+      toggle = 1 - toggle
+
 if __name__ == "__main__":
   joystick = GameportJoystick()
   print("run evtest in another terminal. You have 30 seconds")
@@ -30,4 +39,6 @@ if __name__ == "__main__":
   print("too late. Pressing trigger button.")
   joystick.write(e.EV_KEY, e.BTN_TRIGGER, 1)
   print("written to virtual input device")
+  print("Go to retropie and Hit menu button->Configure Input")
+  joystick.pressAndHoldTriggerButton()
   sleep(30)
