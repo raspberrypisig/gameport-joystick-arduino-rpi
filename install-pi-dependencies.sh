8fc063292344f0fc5876c3365c610328ab6b35d7
raspberrypisig <raspberrypisig@gmail.com>
2018-09-23 16:33:10 +1000

    Create install-pi-dependencies.sh

---
+++ install-pi-dependencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Run this script as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y python-dev python-pip gcc linux-headers-$(uname -r)
+pip install evdev
+
